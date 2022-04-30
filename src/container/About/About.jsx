e1ab8822432c4aebd3e6c27f0778749655eb4f70
allenaxie <allenaxie1@gmail.com>
2022-04-29 22:47:38 -0700

    Create and implement higher order component 'AppWrap' to header and about section.

--- src/container/About/About.jsx
+++ src/container/About/About.jsx
@@ -1,8 +1,8 @@
 import React, {useState, useEffect} from 'react';
 import {motion} from 'framer-motion';
 import './About.scss';
-import { images } from '../../constants';
 import { urlFor, client } from '../../client';
+import {AppWrap} from '../../wrapper';
 
 const About = () => {
   const [abouts, setAbouts] = useState([]);
@@ -43,7 +43,7 @@ const About = () => {
   )
 }
 
-export default About;
+export default AppWrap(About, 'about');
 
 
 
