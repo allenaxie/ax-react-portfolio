6b9cc3e6844a19b1f3030233356bbe9f265a5399
allenaxie <allenaxie1@gmail.com>
2022-05-03 11:24:19 -0700

    Update social media links.

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -1,18 +1,22 @@
 import React from 'react';
-import { BsTwitter, BsInstagram } from 'react-icons/bs';
+import { BsInstagram, BsGithub } from 'react-icons/bs';
 import { FaFacebookF} from 'react-icons/fa';
 
 const SocialMedia = () => {
   return (
     <div className="app__social">
         <div>
-            <BsTwitter/>
+          <a href="https://github.com/allenaxie" target="_blank" rel="noreferrer">
+            <BsGithub/>
+          </a>
         </div>
-        <div>
+        {/* <div>
             <FaFacebookF/>
-        </div>
+        </div> */}
         <div>
+          <a href="https://www.instagram.com/allenaxiee/" target="_blank" rel="noreferrer">
             <BsInstagram/>
+          </a>
         </div>
     </div>
   )
